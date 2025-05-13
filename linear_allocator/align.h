432cfd4285a01116999fc6199530f9ec5221f75e
Alv4roVN <alvarovilelanova@gmail.com>
2025-05-13 21:11:35 +0200

    fix: moved declarations to align.h and fixed offset in lalloc

--- linear_allocator/align.h
+++ linear_allocator/align.h
@@ -1,6 +1,20 @@
+#ifndef ALIGN_H
+#define ALIGN_H
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 bool is_power_of_two(size_t n);
 uintptr_t align_forward(uintptr_t ptr, size_t alignment);
+
+typedef struct {
+	unsigned char *line_buffer;
+	size_t line_buffer_length;
+	size_t line_offset;
+} TotalMem;
+
+TotalMem *lalloc_init(TotalMem *memory_space, void *buffer, size_t buffer_length);
+void *lalloc(TotalMem *memory_space, size_t size);
+
+#endif
--- linear_allocator/lalloc.c
+++ linear_allocator/lalloc.c
@@ -8,13 +8,6 @@
 #define DEFAULT_ALIGNMENT (2*sizeof(void *))
 #endif
 
-typedef struct {
-	unsigned char *line_buffer;
-	size_t line_buffer_length;
-	size_t line_offset;
-} TotalMem;
-
-
 TotalMem *lalloc_init(TotalMem *memory_space, void *buffer, size_t buffer_length) {
 	assert(buffer_length > 0);
 	memory_space->line_buffer = (unsigned char *) buffer;
@@ -27,6 +20,7 @@ void *lalloc(TotalMem *memory_space, size_t size) {
 	assert(size > 0);
 	uintptr_t current_offset = (uintptr_t) memory_space->line_buffer + (uintptr_t) memory_space->line_offset;
 	uintptr_t offset = align_forward(current_offset, DEFAULT_ALIGNMENT);
+	offset -= (uintptr_t)memory_space->line_buffer;
 	/* Check that we are not allocating memory over the maximum allowed */
 	if (memory_space->line_offset + size <= memory_space->line_buffer_length) {
 		void *ptr = &memory_space->line_buffer[offset];
